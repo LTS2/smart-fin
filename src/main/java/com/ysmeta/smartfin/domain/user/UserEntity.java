075ed0e8797c403acee1b60f99dbf868e61cbf2d
LTS2 <leewoojin98@naver.com>
2024-08-12 01:07:29 +0900

    fix: UserEntity 에서 PasswordEntity 제거
    UserEntity 와 PasswordEntity 가 서로 연관관계로 있을 이유가 없다고
    판단되어 제거함.
    PasswordEntity 에서 상태관리 하는 것으로 결정.

--- src/main/java/com/ysmeta/smartfin/domain/user/UserEntity.java
+++ src/main/java/com/ysmeta/smartfin/domain/user/UserEntity.java
@@ -1,13 +1,9 @@
 package com.ysmeta.smartfin.domain.user;
 
 import com.ysmeta.smartfin.common.AbstractBaseEntity;
-import com.ysmeta.smartfin.domain.auth.password.PasswordEntity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -52,10 +48,4 @@ public class UserEntity extends AbstractBaseEntity {
 	 * 토큰
 	 */
 	String token;
-
-	/**
-	 * 비밀번호 엔티티
-	 */
-	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private PasswordEntity passwordEntity;
 }
